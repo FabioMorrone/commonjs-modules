4612917173ddf43585d1fecb8b9888eb8bed8a64
FabioMorrone <morronefabio04@gmail.com>
2025-03-10 17:51:25 +0100

    add

--- names.js
+++ names.js
@@ -8,8 +8,11 @@ const ilNome = nomeCompleto("Fabio", "Morrone")
 console.log(ilNome);
 
 
-function nomeCompleto(Fabio, Morrone){
-    return Fabio + " " + Morrone
+function nomeCompleto(firstName, lastName){
+    return {
+        firstNam: firstName, 
+        lastName: lastName
+    }
 }
 
 
